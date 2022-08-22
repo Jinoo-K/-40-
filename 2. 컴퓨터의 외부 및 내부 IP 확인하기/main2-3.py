8bc3f5ec5e7fa4e3942c9a2adfb622224f65d0ed
Jinoo-K <ruins1608@nate.com>
2022-08-23 01:00:29 +0900

    IP 확인

--- 2. 컴퓨터의 외부 및 내부 IP 확인하기/main2-3.py
+++ 2. 컴퓨터의 외부 및 내부 IP 확인하기/main2-3.py
@@ -1,6 +1,6 @@
-import requests
-import re
+import requests    # 사이트에 접속하기 위해 request 모듈 불러오기
+import re    # IP주소를 찾기 위한 정규식을 사용하기 위해 re 모듈 불러오기
 
-req = requests.get("http://ipconfig.kr")
-out_addr = re.search(r" IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} ", req.text)[1]
-print(out_addr)
+req = requests.get("http://ipconfig.kr")    # http://ipconfig.kr 사이트 접속
+out_addr = re.search(r" IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} ", req.text)[1]    # 정규식 표현 사용, IP 주소를 가져와서 out_addr에 변수 선언
+print(out_addr)    # ip 주소 출력
--- 2. 컴퓨터의 외부 및 내부 IP 확인하기/main2-4.py
+++ 2. 컴퓨터의 외부 및 내부 IP 확인하기/main2-4.py
@@ -1,11 +1,11 @@
-import socket
-import requests
-import re
+import socket    # socket 모듈 불러오기
+import requests    # 사이트에 접속하기 위해 request 모듈 불러오기
+import re    # IP주소를 찾기 위한 정규식을 사용하기 위해 re 모듈 불러오기
 
-in_addr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+in_addr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # 내부 IP 확인 후 출력
 in_addr.connect(("www.google.com", 443))
 print("내부 IP :",in_addr.getsockname()[0])
 
-req = requests.get("http://ipconfig.kr")
+req = requests.get("http://ipconfig.kr")    # 외부 IP 확인 후 출력
 out_addr = re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', req.text)[1]
 print("외부 IP : ",out_addr)
