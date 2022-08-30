53f92b81705f0c46b268a1da4c3dbef186923b78
Jinoo-K <ruins1608@nate.com>
2022-08-31 02:06:13 +0900

    오토마우스 캡처

--- 10. 오토마우스를 활용한 웹페이지 자동화/main10-2.py
+++ 10. 오토마우스를 활용한 웹페이지 자동화/main10-2.py
@@ -6,12 +6,12 @@ import time
 import pyperclip
 
 pyautogui.moveTo(480,367)       # 네이버 홈 화면 검색창 좌표로 이동
-pyautogui.click()       # 클릭
-time.sleep(0.5)     # 대기
+pyautogui.click()
+time.sleep(0.5)
 
 pyperclip.copy("창원 날씨")     # 클립보드에 창원 날씨 저장
 pyautogui.hotkey("ctrl", "v")       # 검색창에 붙여넣기
-time.sleep(0.5)     # 대기
+time.sleep(0.5)
 
-pyautogui.write(["Enter"])      # 엔터키 입력
-time.sleep(1)       # 대기
+pyautogui.write(["Enter"])
+time.sleep(1)
--- 10. 오토마우스를 활용한 웹페이지 자동화/main10-3.py
+++ 10. 오토마우스를 활용한 웹페이지 자동화/main10-3.py
@@ -5,7 +5,7 @@ import pyautogui
 import time
 import pyperclip
 
-pyautogui.moveTo(480,367)
+pyautogui.moveTo(480,367)       # 네이버 홈 화면 검색창 좌표로 이동
 pyautogui.click()
 time.sleep(1)
 
@@ -16,9 +16,9 @@ time.sleep(1)
 pyautogui.write(["Enter"])
 time.sleep(2)
 
-start_x = 51
+start_x = 51        # 캡쳐할 시작 좌표 및 끝 좌표 설정
 start_y = 387
 end_x = 1038
 end_y = 1005
 
-pyautogui.screenshot(r"10. 오토마우스를 활용한 웹페이지 자동화\창원 날씨.png", region=(start_x, start_y, end_x-start_x, end_y-start_y))
+pyautogui.screenshot(r"10. 오토마우스를 활용한 웹페이지 자동화\창원 날씨.png", region=(start_x, start_y, end_x-start_x, end_y-start_y))     # 설정된 좌표로 캡쳐하여 파일 저장
