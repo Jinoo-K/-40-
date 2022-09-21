d2ce8c6d6bc966ec1761d400cbc499784b47d4f6
Jinoo-K <ruins1608@nate.com>
2022-09-21 18:21:09 +0900

    사진 분류

---
+++ 40. 사진에서 사람을 인식하여 분류하기/main40-1.py
@@ -0,0 +1,11 @@
+# pip install torch
+# pip install torchvision
+
+from glob import glob
+
+img_path = r"40. 사진에서 사람을 인식하여 분류하기\원본이미지"
+
+img_list = glob(img_path + "\*.png")
+img_list.extend(glob(img_path + "\*.png"))
+
+print(img_list)
